e65fa532320b96733fe2d4532938de173c502af0
Joanne Lin <y3933y3933@gmail.com>
2025-06-28 00:37:50 +0800

    refactor: set sqlc generate file path to /internal/db/sqlc

--- internal/db/sqlc/games.sql.go
+++ internal/db/sqlc/games.sql.go
@@ -3,7 +3,7 @@
 //   sqlc v1.29.0
 // source: games.sql
 
-package db
+package sqlc
 
 import (
 	"context"
--- internal/db/sqlc/models.go
+++ internal/db/sqlc/models.go
@@ -2,7 +2,7 @@
 // versions:
 //   sqlc v1.29.0
 
-package db
+package sqlc
 
 import (
 	"github.com/jackc/pgx/v5/pgtype"
